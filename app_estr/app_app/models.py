c1ddfcd4cd9dbba9ef72ea0aa6a4accdec346d52
manasses.silva <manasses3mm@gmail.com>
2021-09-09 11:31:31 -0300

    setting unities

--- app_estr/app_app/models.py
+++ app_estr/app_app/models.py
@@ -1,18 +1,28 @@
 from django.conf import settings
-from django.utils import timezone
 from django.db import models
+from django.utils import timezone
 
 
-class Add_Object(models.Model):
+class Unidade(models.Model):
+    
+    CLASSES = [('A', 'Assistência Social'), ('E','Educação'), ('S', 'Saude'), ('EL','Esporte e Lazer'), ('P','Prédios Públicos'), ('M', 'Meio Ambiente'), ('S','Saneamento'), ('C','Cultura'), ('O', 'Outros')]
+    A_UNIDADES = [('CREAS','CREAS'), ('CRAS', 'CRAS'), ('Abrigo', 'Abrigo'), ('Centro POP', 'Centro POP'), ('Centro Dia', 'Centro Dia'), ('Centro de Convivência','Centro de Convivência'), ('Conselho Tutelar', 'Conselho Tutelar'), ('AMA', 'AMA'), ('LAC','LAC'), ('ASA', 'ASA'), ('APAE', 'APAE'), ('APADA', 'APADA'), ('Casa', 'Casa')]
+    
+    
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    classe = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def add(self):
+    lat = models.CharField(max_length=200)
+    long = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=100, blank=True)
+    classe = models.CharField(max_length=100, choices=CLASSES)
+    print(classe.get_choices)
+    unidade = models.CharField(max_length=200)
+    zone = models.CharField(max_length=100, blank=True)
+    
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.name
